402392ebcbd11ffc0cf95fb84f5f5b45f99a7454
sagoresarker <sagoresarker.cse@gmail.com>
2022-10-06 15:34:24 +0600

    Add phone number to Databse(2)

--- mainapp/accounts/forms.py
+++ mainapp/accounts/forms.py
@@ -7,10 +7,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ("username", "email", "gender")
+        fields = ("username", "email", "gender", "phone")
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = ("username", "email", "gender")
+        fields = ("username", "email", "gender", "phone")
